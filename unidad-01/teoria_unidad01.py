b48da8fc65bd932a3a40677178f883ab90deb7af
brendamisu <brendalucia.misu@gmail.com>
2026-04-19 00:05:19 -0300

    Create teoria_unidad01.py

---
+++ unidad-01/teoria_unidad01.py
@@ -0,0 +1,27 @@
+"""
+
+1. Explicar con tus palabras qué es un algoritmo en general y qué es un algoritmo en el contexto de la programación.
+
+Un algoritmo son los pasos a seguir para realizar una tarea o resolver un problema. Es como una receta que incluye 
+los pasos claros y ordenados de lo que hay que hacer para lograr nuestro objetivo.
+En programación, un algoritmo son las instrucciones que luego vamos a escribir en forma de código para que la 
+computadora las ejecute. Antes de empezar a escribir el código siempre tenemos que definir nuestro algoritmo.
+
+
+2. ¿Cuántos tipos de lenguaje de programación hay? Enumerarlos y detallar brevemente cada uno.
+
+Existen dos tipos de lenguaje de programación: los de alto nivel y los de bajo nivel.
+Los lenguajes de alto nivel están basados en el lenguaje humano y son más fáciles de entender para las personas. 
+En cambio, los lenguajes de bajo nivel son más cercanos al lenguaje de la máquina, por lo tanto son más complejos 
+de escribir y difíciles de entender para las personas.
+
+
+3. Detallar las diferencias entre un lenguaje de programación interpretado y uno compilado.
+
+Un lenguaje de programación interpretado es aquel que cuenta con un intérprete que lee, traduce y ejecuta el código 
+línea por línea. En cambio, un lenguaje de programación compilado es aquel que traduce todo el programa al lenguaje 
+de la máquina antes de ejecutarlo y luego genera un archivo que puede ejecutarse directamente, sin necesidad de 
+volver a traducirlo.
+
+
+"""
